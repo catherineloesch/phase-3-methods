553041b992af1da85181720f9491efe95cd4bb9d
Catherine Loesch <catherine.loesch@protonmail.com>
2022-01-21 15:44:52 +0000

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,21 +1,24 @@
-# Your code here!
-
+#greet_programmer
 def greet_programmer
     puts "Hello, programmer!"
 end
 
+#greet
 def greet(name)
     puts "Hello, #{name}!"
 end
 
+#greet_with_default
 def greet_with_default(name="programmer")
     puts "Hello, #{name}!"
 end
 
+#add
 def add(num1, num2)
     num1 + num2
 end
 
+#halve
 def halve(num)
     if num.class != Integer
         return nil
